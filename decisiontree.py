87b30f1590bc374198e025f827e55560e4d0eb3b
Ubuntu <evandrodacosta@gmail.com>
2022-10-14 18:49:12 -0300

    Select x and y arrays. Split values to train test

--- decisiontree.py
+++ decisiontree.py
@@ -4,3 +4,20 @@ import matplotlib.pyplot as plot
 import seaborn as sns
 import pydotplus
 
+dataset = pd.read_csv('wine.data', header=None)
+dataset.columns = ['label',
+                   'alcohol', 
+                   'malic_acid', 
+                   'ash', 
+                   'alcalinity_of_ash', 
+                   'magnesium', 
+                   'total_phenols', 
+                   'flavanoids', 
+                   'nonflavanoid_phenols', 
+                   'proanthocyanins', 
+                   'color_intensity', 
+                   'hue',
+                   'OD280/OD315',
+                   'proline'
+                   ]
+print(dataset.head())
